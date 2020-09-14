fe84e22330c9d29fab9ed08ec613233b02a52363
worning77 <worning77@sina.cn>
2020-09-13 23:06:03 -0500

    add hero style and loading animations

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*menu show*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+};
+
+showMenu('nav-toggled', 'nav-menu-open');
+
+/*animated headlines */
+
+// OVERLAY
+
