0c3b5678b808ac9eab5effda089774fd5656a67c
worning77 <worning77@sina.cn>
2020-09-16 17:30:32 -0500

    add cursor effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,41 @@ showMenu('nav-toggled', 'nav-menu-open');
 
 // OVERLAY
 
+//Cursor
+
+const mouseCursor = document.querySelector('.cursor');
+const navLinks = document.querySelectorAll('.l-header a');
+const socialIcons = document.querySelectorAll('.hero-social ion-icon');
+
+//customized cursor
+window.addEventListener(
+  'mousemove',
+  (cursor = (e) => {
+    mouseCursor.style.top = `${e.pageY}px`;
+    mouseCursor.style.left = `${e.pageX}px`;
+  })
+);
+
+//header hover effect
+navLinks.forEach((link) => {
+  link.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+    link.classList.add('hovered-link');
+  });
+  link.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+    link.classList.remove('hovered-link');
+  });
+});
+
+//social icon hover effect
+socialIcons.forEach((icon) => {
+  icon.addEventListener('mouseover', () => {
+    mouseCursor.classList.add('link-grow');
+    icon.classList.add('hovered-icon');
+  });
+  icon.addEventListener('mouseleave', () => {
+    mouseCursor.classList.remove('link-grow');
+    icon.classList.remove('hovered-icon');
+  });
+});
